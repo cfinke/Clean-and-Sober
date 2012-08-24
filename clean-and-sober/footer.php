b68198e19ab4fe714fb955fd27b6da58bd6a5eee
Christopher Finke <cfinke@gmail.com>
2012-08-24 06:08:03 +0000

    Formatting.

--- clean-and-sober/footer.php
+++ clean-and-sober/footer.php
@@ -1,25 +1,13 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the id=main div and all content after
- *
- * @package Clean and Sober
- * @since Clean and Sober 1.0
- */
-?>
+			</div>
 
-	</div><!-- #main -->
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="site-info">
+					<?php do_action( 'clean_and_sober_credits' ); ?>
+					<p><a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'clean-and-sober' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'clean-and-sober' ), 'WordPress' ); ?></a></p>
+				</div>
+			</footer>
+		</div>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'clean_and_sober_credits' ); ?>
-			<p><a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'clean-and-sober' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'clean-and-sober' ), 'WordPress' ); ?></a></p>
-		</div><!-- .site-info -->
-	</footer><!-- .site-footer .site-footer -->
-</div><!-- #page .hfeed .site -->
-
-<?php wp_footer(); ?>
-
-</body>
+		<?php wp_footer(); ?>
+	</body>
 </html>
--- clean-and-sober/searchform.php
+++ clean-and-sober/searchform.php
@@ -1,13 +1,5 @@
-<?php
-/**
- * The template for displaying search forms in Clean and Sober
- *
- * @package Clean and Sober
- * @since Clean and Sober 1.0
- */
-?>
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-		<label for="s" class="assistive-text"><?php _e( 'Search', 'clean-and-sober' ); ?></label>
-		<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'clean-and-sober' ); ?>" />
-		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'clean-and-sober' ); ?>" />
-	</form>
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+	<label for="s" class="assistive-text"><?php _e( 'Search', 'clean-and-sober' ); ?></label>
+	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'clean-and-sober' ); ?>" />
+	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'clean-and-sober' ); ?>" />
+</form>
