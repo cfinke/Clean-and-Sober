86ffc23262ffa8b830329d0bfb2846a4b36431eb
Christopher Finke <cfinke@gmail.com>
2012-08-29 02:59:32 +0000

    Adding postmeta

--- clean-and-sober/content-single.php
+++ clean-and-sober/content-single.php
@@ -18,4 +18,35 @@
 		<?php the_content(); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'clean-and-sober' ), 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
+	
+	<footer class="entry-meta">
+		<?php if ( 'post' == get_post_type() ) { ?>
+			<ol>
+				<?php
+			
+				$categories_list = get_the_category_list( __( ', ', 'clean-and-sober' ) );
+				
+				if ( $categories_list && clean_and_sober_categorized_blog() ) {
+					?>
+					<li class="cat-links">
+						<?php printf( __( 'Posted in %1$s', 'clean-and-sober' ), $categories_list ); ?>
+					</li>
+				<?php } ?>
+				
+				<?php
+			
+				/* translators: used between list items, there is a space after the comma */
+				$tags_list = get_the_tag_list( '', __( ', ', 'clean-and-sober' ) );
+			
+				if ( $tags_list ) {
+					?>
+					<li class="tag-links">
+						<?php printf( __( 'Tagged %1$s', 'clean-and-sober' ), $tags_list ); ?>
+					</li>
+				<?php } ?>
+				
+				<?php edit_post_link( __( 'Edit', 'clean-and-sober' ), '<li class="edit-link">', '</li>' ); ?>
+			</ol>
+		<?php } ?>
+	</footer><!-- #entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
