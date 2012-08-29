81f93b3e090c207cc0115ba7cedd2b0ef265d666
Christopher Finke <cfinke@gmail.com>
2012-08-29 03:00:30 +0000

    Adding searchbox to bottom navigation

--- clean-and-sober/inc/template-tags.php
+++ clean-and-sober/inc/template-tags.php
@@ -26,11 +26,14 @@ function clean_and_sober_content_nav( $nav_id ) {
 
 	?>
 	<nav role="navigation" id="<?php echo $nav_id; ?>" class="<?php echo $nav_class; ?>">
-		<h1 class="assistive-text"><?php _e( 'Post navigation', 'clean-and-sober' ); ?></h1>
+		<!--<h1 class="assistive-text"><?php _e( 'Post navigation', 'clean-and-sober' ); ?></h1>-->
 
 	<?php if ( is_single() ) : // navigation links for single posts ?>
 
 		<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'clean-and-sober' ) . '</span> %title' ); ?>
+		<div class="search">
+			<?php get_template_part( 'searchform' ); ?>
+		</div>
 		<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'clean-and-sober' ) . '</span>' ); ?>
 
 	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
@@ -39,9 +42,15 @@ function clean_and_sober_content_nav( $nav_id ) {
 		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'clean-and-sober' ) ); ?></div>
 		<?php endif; ?>
 
-		<?php if ( get_previous_posts_link() ) : ?>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'clean-and-sober' ) ); ?></div>
-		<?php endif; ?>
+		<div class="search">
+			<?php get_template_part( 'searchform' ); ?>
+		</div>
+
+		<div class="nav-next">
+			<?php if ( get_previous_posts_link() ) : ?>
+				<?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'clean-and-sober' ) ); ?>
+			<?php endif; ?>
+		</div>
 
 	<?php endif; ?>
 
--- clean-and-sober/searchform.php
+++ clean-and-sober/searchform.php
@@ -1,5 +1,4 @@
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<label for="s" class="assistive-text"><?php _e( 'Search', 'clean-and-sober' ); ?></label>
-	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'clean-and-sober' ); ?>" />
+	<input type="text" class="field" name="s" id="s" />
 	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'clean-and-sober' ); ?>" />
 </form>
